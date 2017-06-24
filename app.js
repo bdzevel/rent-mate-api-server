6f810fb53a851814cb3eb6e7904837e52bab22d2
bdzevel <bdzevel@gmail.com>
2017-06-24 13:53:50 -0400

    Remove test code

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const data = require('rent-mate-data');
 data.connect(process.env.MONGO_URL)
 .then(function(models) {
     global.models = models;
-    const User = global.models.user;
-    const me = new User({ username: 'bdzevel', password: '123456', firstName: 'Boris', lastName: 'Dzevel' });
-    return me.save();
 })
 .catch(function(err) {
     console.error(' > ERR!', err);
