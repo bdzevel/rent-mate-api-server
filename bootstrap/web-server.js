8f90e4c658efb9d6b52f634d95d75f2b8017803a
Boris Dzevel <bdzevel@gmail.com>
2017-06-29 23:36:51 -0400

    Slight refactor

--- bootstrap/web-server.js
+++ bootstrap/web-server.js
@@ -7,14 +7,14 @@ const routes = require('../routes/index');
 
 const app = express();
 app.use(session({
+  resave: false,
+  saveUninitialized: false,
   secret: process.env.SESSION_SECRET,
   cookie: {
     path: '/',
     secure: process.env.ENVIRONMENT === 'production',
     maxAge: 1000 * 60 * 60 * 24 * 60, // 60 days
   },
-  saveUninitialized: false,
-  resave: false,
 }));
 authentication.initialize(app);
 authorization.initialize();
